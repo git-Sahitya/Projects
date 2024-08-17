016a18097794c90bb621c636e6d91fae41fa4bcb
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-08-17 11:15:51 +0530

    Add files via upload
    iValidate my email and you!

---
+++ emailValidator/js/index.js
@@ -0,0 +1,43 @@
+
+let result = {
+
+  "tag": "",
+  "free": true,
+  "role": false,
+  "user": "sahitya5289",
+  "email": "sahitya5289@gmail.com",
+  "score": 0.64,
+  "state": "deliverable",
+  "domain": "gmail.com",
+  "reason": "valid_mailbox",
+  "mx_found": true,
+  "catch_all": null,
+  "disposable": false,
+  "smtp_check": true,
+  "did_you_mean": "",
+  "format_valid": true
+}
+
+submitBtn.addEventListener("click", async(e) => {
+  e.preventDefault()
+  console.log("clicked");     
+  resultCont.innerHTML = ' <img width="123" src="img/loading.svg" alt="">'
+
+  let key = "ema_live_MARUzn8fp0Gaj3rRUN6v6GFIrf0JClnS0eT5Ne3N"
+  let email = document.getElementById('username').value
+  let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+  let res = await fetch(url)
+  let result = await res.json()
+
+
+  let str = ``
+  for (key of Object.keys(result)) {
+    if(result[key] !=="" && result[key] !== " "){
+    str = str + `<div>${key} : ${result[key]}</div>`
+    }
+  }
+  console.log(str)
+  resultCont.innerHTML = str
+})
+
+
