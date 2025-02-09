22ec4d1452d41a88b3b05f3659d3337827451422
git-Sahitya <sahitya5289@gmail.com>
2025-02-09 18:19:38 +0530

    Add owner creation route for development and update router responses

--- SCATCH/routes/ownersRouter.js
+++ SCATCH/routes/ownersRouter.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const ownerModel = require("../models/owners.model.js")
+const ownerModel = require("../models/owners.model.js");
 
-router.get("/", (req,res) => {
-  res.send("Router Working!!");
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owners = await ownerModel.find();
+
+    if (owners.length > 0) {
+      return res
+        .status(503)
+        .send("You don't have permission to create a new owner. ");
+    }
+    let { fullname, email, password } = req.body;
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+    res.status(201).send(createdOwner);
+  });
+}
+
+router.get("/", (req, res) => {
+  res.send("ownerRouter Working!!");
 });
 
+//console.log((process.env.NODE_ENV));
+
 module.exports = router;
+
+// set node environment
+
+//  $env:NODE_ENV="development"
