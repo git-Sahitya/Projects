f57293edffed1a5e968d18e45e7f36a3cea53231
git-Sahitya <sahitya5289@gmail.com>
2025-02-09 08:51:07 +0530

    Refactor project structure: add mongoose connection, models, and routers; remove unnecessary .gitkeep files; update package.json with prettier

--- SCATCH/index.js
+++ SCATCH/index.js
@@ -1,8 +1,20 @@
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
+const path = require('path')
+const db = require('./config/mongoose.connection')
+const ownersRouter = require('./routes/ownersRouter.js')
+const productsRouter = require('./routes/productsRouter.js')
+const usersRouter = require('./routes/usersRouter.js')
 
-app.get('/' , (req,res)=>{
-    res.send("Hey There ")
-})
+app.use(express.json())
+app.use(express.urlencoded({extended :true}))
+app.use(express.static(path.join(__dirname , "public")))
+app.use(cookieParser())
+app.set ("view engine" , "ejs")
+
+app.use('/owners' , ownersRouter)
+app.use('/users' , usersRouter)
+app.use('/products' , productsRouter)
 
 app.listen(3000)
