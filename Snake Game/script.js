a4c7f32888f68e1fca0881ef9fb4af3a45be602f
git-Sahitya <sahitya5289@gmail.com>
2026-01-06 20:19:14 +0530

    Refactor Snake Game: Update block dimensions to 50px, enhance rendering logic, and apply fill color to snake segments

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,13 +1,49 @@
 const board = document.querySelector(".board")
-const blockHeight = 30
-const blockWidth = 30
+const blockHeight = 50
+const blockWidth = 50
 
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 
-for (let i = 0; i < rows*cols; i++) {
-   const block = document.createElement("div")
-   block.classList.add("block")
-   board.appendChild(block)
-    
+const blocks = []
+const snake = [
+
+    {
+        x: 1, y: 3
+    }]
+let direction = 'right'
+
+
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        const block = document.createElement("div")
+        block.classList.add("block")
+        board.appendChild(block)
+        block.innerText = `${row}-${col}`
+        blocks[`${row}-${col}`] = block
+    }
 }
+// Write a function to render a segments
+function render() {
+    snake.forEach((segment) => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+}
+
+setInterval(() => {
+    //Calculate the direction of snake 
+    let head = null
+
+    if (direction === "left") {
+        head = { x: snake[0].x, y: snake[0].y - 1 }
+    }
+
+    snake.forEach((segment) => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+    })
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 400)
