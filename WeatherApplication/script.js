660b4d17d75e02dd62a85b7f07638f6bf59e820d
git-Sahitya <sahitya5289@gmail.com>
2026-01-13 20:37:40 +0530

    Update API key placeholder in WeatherApplication script

--- WeatherApplication/script.js
+++ WeatherApplication/script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector(".weather-body")
 
 
 async function checkWeather(city) {
-    const api_key = "f2485912523735ff02a9c855b2085c30"
+    const api_key = "Add_your_own_key"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json())
